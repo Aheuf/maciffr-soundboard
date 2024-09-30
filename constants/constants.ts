35e5e01f272274f2cf5d0934754b147156dadf7c
clement_genty <84876568+Aheuf@users.noreply.github.com>
2024-09-30 08:18:30 +0000

    ajout: sons

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,4 @@
-export const sounds: string[] = ["huh","dlaMerde","aaaahhhh","combien","gendarme","hendek","plante"]
+export const sounds: string[] = ["huh","dlaMerde","aaaahhhh","combien","gendarme","hendek","plante","ouaisouaisouais","menfin","yeet"]
 
 export const PATH = {
     ASSET_SOUND:"../assets/sounds/",
