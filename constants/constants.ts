49da642069655d70265e50fe5dd3236318889857
clement_genty <84876568+Aheuf@users.noreply.github.com>
2024-10-02 12:08:46 +0000

    feat: implementation des sons brux

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,5 +1,38 @@
-export const sounds: string[] = []
+export const sounds: string[] = [
+    "aaaahhhh",
+    "combien",
+    "dlaMerde",
+    "gendarme",
+    "hendek",
+    "huh",
+    "menfin",
+    "ouaisouaisouais",
+    "plante",
+    "yeet"
+]
 
-export const audios = [];
+export const audios = [
+    require("../assets/audios/aaaahhhh.mp3"),
+    require("../assets/audios/combien.mp3"),
+    require("../assets/audios/dlaMerde.mp3"),
+    require("../assets/audios/gendarme.mp3"),
+    require("../assets/audios/hendek.mp3"),
+    require("../assets/audios/huh.mp3"),
+    require("../assets/audios/menfin.mp3"),
+    require("../assets/audios/ouaisouaisouais.mp3"),
+    require("../assets/audios/plante.mp3"),
+    require("../assets/audios/yeet.mp3")
+];
 
-export const images = [];
+export const images = [
+    require("../assets/images/aaaahhhh.png"),
+    require("../assets/images/combien.png"),
+    require("../assets/images/dlaMerde.png"),
+    require("../assets/images/gendarme.png"),
+    require("../assets/images/hendek.png"),
+    require("../assets/images/huh.png"),
+    require("../assets/images/menfin.png"),
+    require("../assets/images/ouaisouaisouais.png"),
+    require("../assets/images/plante.png"),
+    require("../assets/images/yeet.png")
+];
